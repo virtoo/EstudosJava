6243766d7a22215a53ef8242f898b27d79e883c3
virtoo <victorhfidelis@gmail.com>
2023-06-12 17:40:28 -0300

    primeiro commit

---
+++ src/aulas/aula10/Variaveis.java
@@ -0,0 +1,10 @@
+package aulas.aula10;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        int idade = 21;
+        String nome = "Victor";
+        System.out.println("Idade = " + idade);
+        System.out.println("Nome = " + nome);
+    }
+}
