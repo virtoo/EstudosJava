09d997baae0d6e27abad851cec159355dacda02e
virtoo <victorhfidelis@gmail.com>
2023-06-25 21:54:41 -0300

    continuação dos exercicios

--- src/aulas/Vetores.java
+++ src/aulas/Vetores.java
@@ -12,5 +12,14 @@ public class Vetores {
         System.out.println("A temperatura do dia 4 é: " + temperatura[3]);
         System.out.println("O tamanho do Array é: " + temperatura.length);
         System.out.println("Espaço em memória do Array: " + temperatura);
+        System.out.println("=============================================");
+        System.out.println("Valor da temperatura:");
+        for (int i = 0; i < temperatura.length; i++){
+            System.out.println("A temperatura do dia " + (i+1) + " é: " + temperatura[i]);
+        }
+        System.out.println("=============================================");
+        for (double temp : temperatura){
+            System.out.println(temp);
+        }
     }
 }
---
+++ src/exercicios/Exercicios_15.java
@@ -0,0 +1,110 @@
+package exercicios;
+import java.util.Scanner;
+
+public class Exercicios_15 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+//        ====== maior número ======
+//        System.out.println("Escreva um número:");
+//        int num1 = scan.nextInt();
+//        System.out.println("Escreva outro número:");
+//        int num2 = scan.nextInt();
+//        if (num1 > num2){
+//            System.out.println("O número maior é: " + num1);
+//        } else {
+//            System.out.println("O número maior é: " + num2);
+//        }
+
+//        ====== valor é positivo ou negativo ======
+//        System.out.println("Escreva um número positivo ou negativo:");
+//        int n = scan.nextInt();
+//        if (n > 0){
+//            System.out.println("O número é positivo");
+//        } else if (n == 0){
+//            System.out.println("O número é neutro");
+//        } else {
+//            System.out.println("O número é negativo");
+//        }
+
+//        ====== letra digitada F ou M ======
+//        System.out.println("Escreva uma letra (F ou M)");
+//        String input = scan.next();
+//        if (input.equalsIgnoreCase("f")){
+//            System.out.println("F - Feminino");
+//        } else if (input.equalsIgnoreCase("m")){
+//            System.out.println("M - Masculino");
+//        } else {
+//            System.out.println("Sexo Inválido");
+//        }
+
+//        ====== Letra digitada é vogal ou consoante ======
+//        System.out.println("Escreva uma letra:");
+//        String l = scan.next();
+//        switch (l){
+//            case "a":
+//            case "e":
+//            case "i":
+//            case "o":
+//            case "u":
+//                System.out.println("É uma vogal");
+//                break;
+//            default:
+//                System.out.println("É uma consoante");
+//        }
+
+//        ====== média do aluno ======
+//        System.out.println("Entre com a primeira nota do aluno:");
+//        double nota1 = scan.nextDouble();
+//        System.out.println("Entre com a segunda nota do aluno:");
+//        double nota2 = scan.nextDouble();
+//        double media = (nota1 + nota2) / 2;
+//        if (media == 10){
+//            System.out.println("Aprovado com Distinção!!!");
+//        } else if (media >= 7){
+//            System.out.println("Aprovado!!!");
+//        } else {
+//            System.out.println("Reprovado!!!");
+//        }
+
+//        ====== leitura de três números e o maior deles ======
+//        System.out.println("Entre com três números: ");
+//        int a,b,c;
+//        a = scan.nextInt();
+//        b = scan.nextInt();
+//        c = scan.nextInt();
+//        if (a>b && a>c){
+//            System.out.println("O num1 é maior: " + a);
+//        } else if (b>a && b>c) {
+//            System.out.println("O num2 é maior: " + b);
+//        } else {
+//            System.out.println("O num3 é maior: " + c);
+//        }
+
+//        ====== leitura de três números e o maior e menor deles ======
+//        System.out.println("Entre com três números: ");
+//        int a = scan.nextInt();
+//        int b = scan.nextInt();
+//        int c = scan.nextInt();
+//        === menor ===
+//         if (a<b && a<c){
+//            System.out.println("O num1 é menor: " + a);
+//         } else if (b<a && b<c) {
+//            System.out.println("O num2 é menor: " + b);
+//         } else {
+//            System.out.println("O num3 é menor: " + c);
+//         }
+//        === maior ===
+//        if (a>b && a>c){
+//            System.out.println("O num1 é maior: " + a);
+//        } else if (b>a && b>c) {
+//            System.out.println("O num2 é maior: " + b);
+//        } else {
+//            System.out.println("O num3 é maior: " + c);
+//        }
+
+//        ====== leitura de preço três produtos e o mais barato deles ======
+
+
+//        ====== números em ordem decrescente ======
+    }
+}
